7c3665bde013c9209753fc51dfdbddf2fedb2020
Hannamaa <hanna.m.valli@gmail.com>
2020-03-06 11:18:32 +0200

    ylimääräinen säälinkki poistettu Homesta

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,6 @@ export const Home = () => {
       <Weather/>
       <Jumbotron>
         <h1>Welcome to the Campus Forum ™</h1>
-        <a class="weatherwidget-io" href="https://forecast7.com/en/60d2124d66/espoo/" data-label_1="ESPOO" data-label_2="WEATHER" data-theme="original" >Check the weather in Keilaniemi &#x2600; &#9928;</a>
       </Jumbotron>
       <div>
         <Tab.Container id='list-group-tabs-mods' defaultActiveKey='#link1'>
